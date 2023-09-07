c8302bace3ca5d2d67e72789e60faf4fd1fa9841
drswith <540628938@qq.com>
2023-09-07 11:03:50 +0800

    chore: update eslint

--- packages/express-modules/src/config/index.ts
+++ packages/express-modules/src/config/index.ts
@@ -1,3 +1,4 @@
+import process from 'node:process'
 import { KukaEnv } from '@kukafe/fe-utils'
 import type { KukaEnvType } from '@kukafe/fe-utils'
 
--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -1,3 +1,4 @@
+import process from 'node:process'
 import { defineConfig } from 'tsup'
 
 const env = process.env.NODE_ENV
